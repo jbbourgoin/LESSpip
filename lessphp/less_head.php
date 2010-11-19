1b1c3821f8b762f4cda09bedc62159fecf576f52
jbbourgoin <jbbourgoin@pabusienne.(none)>
2010-11-19 15:19:30 +0100

    lesspip moved !

--- lessphp/less_head.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function lesspip_insert_head($texte) 
-{
-  $texte .= '<link rel="stylesheet" href="'.find_in_path('habillage_less.css').'" type="text/css" media="projection, screen, tv" />';
-  return $texte;
-}
-
-?>
